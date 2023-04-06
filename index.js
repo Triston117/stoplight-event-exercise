b461331e64dee05adebf95953f1e2c6eb8f229e7
Triston117 <roviiknaris@yahoo.com>
2023-04-06 15:32:13 -0700

    Holy crap it works

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-(function() {
-  'use strict';
-
-  // YOUR CODE HERE
-})();
+let redButton = document.getElementById("stopButton");
+let yellowButton = document.getElementById("slowButton");
+let greenButton = document.getElementById("goButton");
+let redLight = document.getElementById("stopLight");
+let yellowLight = document.getElementById("slowLight");
+let greenLight = document.getElementById("goLight");
+console.log(redButton, redLight);
+redButton.addEventListener("click", function () {
+  redLight.classList.toggle("stop");
+});
+redButton.addEventListener("mouseenter", function () {
+  console.log(`entered ${stopButton.textContent} button`);
+});
+redButton.addEventListener("mouseleave", function () {
+  console.log(`left ${stopButton.textContent} button`);
+});
+yellowButton.addEventListener("click", function () {
+  yellowLight.classList.toggle("slow");
+});
+yellowButton.addEventListener("mouseenter", function () {
+  console.log(`entered ${slowButton.textContent} button`);
+});
+yellowButton.addEventListener("mouseleave", function () {
+  console.log(`left ${slowButton.textContent} button`);
+});
+greenButton.addEventListener("click", function () {
+  greenLight.classList.toggle("go");
+});
+greenButton.addEventListener("mouseenter", function () {
+  console.log(`entered ${goButton.textContent} button`);
+});
+greenButton.addEventListener("mouseleave", function () {
+  console.log(`left ${goButton.textContent} button`);
+});
+//Holy crap this works
